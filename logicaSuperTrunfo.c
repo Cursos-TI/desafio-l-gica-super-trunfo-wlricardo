485aaf4eb6c82c78a810b36eaaa3347ad0aea77e
Willem de Lima Ricardo <42807321+wlricardo@users.noreply.github.com>
2025-08-15 02:16:33 +0000

    Implementando as funções limpar_tela() e esperar_dois_milissegundos

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -23,6 +23,16 @@ int limpar_buffer() {
     return 0;
 }
 
+// Temporizador de 2.000 milissegundos (2 segundos)
+void esperar_dois_milissegundos()
+{
+    #ifdef _WIN32
+        Sleep(2000);
+    #else
+        sleep(2);
+    #endif
+}
+
 // Limpar tela
 void limpar_tela()
 {
@@ -32,6 +42,13 @@ void limpar_tela()
         system("clear");
     #endif
 }
+
+
+/**
+ * 
+ * PROGRAMA PRINCIPAL
+ * 
+ */
 
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
@@ -48,44 +65,48 @@ int main() {
     printf("+++ CARTA 1 +++\n");
 
     /* Estado */
-    printf("\nEstado : ");
+    printf("\nEstado....................: ");    
     fgets(estado1, MAX, stdin);
     estado1[strcspn(estado1, "\n")] = '\0';       // Remove o \n do final
 
     /* Código */
-    printf("Código : ");
+    printf("Código....................: ");
     fgets(codigo_carta1, MAX, stdin);
     codigo_carta1[strcspn(codigo_carta1, "\n")] = '\0';
 
     /* Cidade */
-    printf("Cidade : ");
+    printf("Cidade....................: ");    
     fgets(cidade1, MAX, stdin);
     cidade1[strcspn(cidade1, "\n")] = '\0';
 
     /* População */
-    printf("População : ");
+    printf("População.................: ");
     scanf("%d", &populacao1);
 
     /* Área (em km²)*/
-    printf("Área (em km²) : ");
+    printf("Área (em km²).............: ");
     scanf("%f", &area1);
 
-    /* PIB (em bilhões)*/
-    printf("PIB (em bilhões) : ");
+    /* PIB (em bilhões)*/    
+    printf("PIB (em bilhões)..........: ");
     scanf("%f", &pib1);
 
-    printf("Qtd de pontos turísticos: ");
+    printf("Qtd de pontos turísticos..: ");
     scanf("%d", &qtd_pontos_turisticos1);
 
+    printf("\n\t ++ Dados cadastrados com sucesso ++\n");
+    esperar_dois_milissegundos();
+    limpar_tela();
+
     // Exibindo os dados lidos (opcional, para verificar)
-    printf("\n--- Dados da Carta 1 ---\n");
-    printf("Estado: %s\n", estado1);
-    printf("Código da carta: %s\n", codigo_carta1);
-    printf("Cidade: %s\n", cidade1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %.2f km²\n", area1);
-    printf("PIB: %.2f\n", pib1);
-    printf("Pontos turísticos: %d\n", qtd_pontos_turisticos1);
+    printf("--- Dados da Carta 1 ---\n");
+    printf("-Estado.............: %s\n", estado1);
+    printf("-Código da carta....: %s\n", codigo_carta1);
+    printf("-Cidade.............: %s\n", cidade1);
+    printf("-População..........: %d\n", populacao1);
+    printf("-Área...............: %.2f km²\n", area1);    
+    printf("-PIB................: %.2f\n", pib1);
+    printf("-Pontos turísticos..: %d\n", qtd_pontos_turisticos1);
 
     return 0;
 }
